67712ac4d12240a86465d439fa108366ab36cce6
udhaya-2k <udhayakumarasamy18@gmail.com>
2023-02-20 12:12:31 +0530

    adding springboot

---
+++ src/test/java/com/tarus/tarus/TarusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarus.tarus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
