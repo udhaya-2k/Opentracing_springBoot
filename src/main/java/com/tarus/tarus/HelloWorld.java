9144f2922dbe04511767d6f0c2a36940ea5553eb
tarus <tarus@tarus.com>
2023-02-24 23:40:45 +0530

    Get Mapping

--- src/main/java/com/tarus/tarus/HelloWorld.java
+++ src/main/java/com/tarus/tarus/HelloWorld.java
@@ -1,12 +1,24 @@
 package com.tarus.tarus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
 @RestController
 public class HelloWorld
 {
-@RequestMapping("/hello_request")
+@RequestMapping("/home")
 public String hello()
 {
-return "Remote Server: Hello";
+return "Welcome to the HomePage";
 }
+
+@GetMapping(value = "/hello_request")
+private String getHello() {
+String uri = "http://192.168.56.103:9100/hello_request";
+RestTemplate restTemplate = new RestTemplate();
+String result = restTemplate.getForObject(uri, String.class);
+return result;
+}
+
 }
